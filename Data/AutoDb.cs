bf971b047be48bcb2b2c52d286e6f87d17af782a
Nathalia <natylafosse@gmail.com>
2020-01-17 23:07:01 -0500

    Add project files.

---
+++ Data/AutoDb.cs
@@ -0,0 +1,12 @@
+﻿using AutoShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoShop.Data
+{
+    public class AutoDb : DbContext
+    {
+        
+        public AutoDb(DbContextOptions<AutoDb> options) : base(options){ }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Migrations/20200117232006_InitialDBCreation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoShop.Migrations
+{
+    public partial class InitialDBCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
